f3177fff2aebcd2547b62976a706c8f451660d64
Guilherme <gbletsch@gmail.com>
2020-05-24 10:48:26 -0300

    .

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -11,34 +11,46 @@ import { FiMenu } from 'react-icons/fi'
 
 function Toolbar() {
   return (
-    <div className='toolbar'>
-        <section className="toolbar__first-row">
-          <ul>
-            <li><img src={YoutubeLogo} alt=""/></li>
-            <li><img src={BitchuteLogo} alt=""/></li>
-            <li><img src={FacebookLogo} alt=""/></li>
-            <li><img src={MindsLogo} alt=""/></li>
-            <li><img src={TwitterLogo} alt=""/></li>
-            <li><img src={GabLogo} alt=""/></li>
-          </ul>
-        </section>
+    <header className='toolbar'>
+        <ul className='toolbar__icons'>
+          <li><img src={YoutubeLogo} alt=""/></li>
+          <li><img src={BitchuteLogo} alt=""/></li>
+          <li><img src={FacebookLogo} alt=""/></li>
+          <li><img src={MindsLogo} alt=""/></li>
+          <li><img src={TwitterLogo} alt=""/></li>
+          <li><img src={GabLogo} alt=""/></li>
+        </ul>
         <section className="toolbar__second-row">
           <a href="/"><img src={AncapsuLogo} alt="AncapSu" /></a>
-        </section>
-        <section className="toolbar__third-row">
           <ul>
             <li><a className='button' href="/login">Autenticar</a></li>
             <li><a className='button' href="/create-account">Criar conta</a></li>
           </ul>
+        </section>
+        <input type="checkbox" id='menu-btn' className="menu-btn"/>
+        <label htmlFor="menu-btn" className="menu-icon">
+          <FiMenu size='25px' color='black' />
+        </label>
+        <ul className='toolbar__menu'>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">videos</a></li>
+            <li><a href="/">matérias</a></li>
+            <li><a href="/">pautas</a></li>
+            <li><a href="/">minha conta</a></li>
+        </ul>
+
+
+
+
+        {/* <section className="toolbar__third-row">
           <div className="toolbar__right">
             <a href="#menu" className="button">
-              <FiMenu size='25px' color='black' />            </a>
+              </a>
           </div>
         </section>
         <section className="toolbar__forth-row">
-          
-        </section>
-    </div>
+        </section> */}
+    </header>
   )
 }
 
