368a3d763599d3aad9ed75bc6e761adbcc8806c3
Guilherme <gbletsch@gmail.com>
2020-06-07 11:21:14 -0300

    testing with local backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_ENDPOINT = process.env.REACT_APP_AXIOS_URL;
-console.log("API", API_ENDPOINT);
+// console.log("API", API_ENDPOINT);
 
 const api = axios.create({
   baseURL: API_ENDPOINT,
